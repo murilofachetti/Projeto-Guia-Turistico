084d2692073d06738ecd94ee72c08a499b7e5c17
Murilo Fachetti <110673100+murilofachetti@users.noreply.github.com>
2022-09-21 20:35:50 -0300

    Atualização de Projeto
    Adicionado Placeholder de sobreposição em carregamento ao projeto.

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,12 @@
+// PRÉ LOADER
+
+$(window).on('load', function() {
+  $('#preloader .inner').fadeOut();
+  $('#preloader').delay(350).fadeOut('slow');
+  $('body').delay(350).css({'overflow': 'visible'});
+})
+// PRÉ LOADER
+
 // Config Slick
 $(document).ready(function(){
 $('.listaturismo').slick({
